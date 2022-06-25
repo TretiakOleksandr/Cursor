f60f117797462d7a6fc5a719eebdfc3682f17c78
Tretiak Oleksandr <zukaramba@gamil.com>
2022-06-25 14:59:06 +0300

    Without 6 and 7

--- HW4/hw4.py
+++ HW4/hw4.py
@@ -1,3 +1,4 @@
+#1
 class Vehicle:
     def __init__(self, name, max_speed, total_capacity):
         self.name = name
@@ -7,31 +8,59 @@ class Vehicle:
     def fare(self):
         return self.total_capacity * 100
 
+#2
 class Bus(Vehicle):
-    def __init__(self, name, max_speed, total_capacity):
+    def __init__(self, name, max_speed, total_capacity, used_capacity): #6
         super().__init__(name, max_speed, total_capacity)
+        self.used_capacity = used_capacity
 
+    def __str__(self):
+        return f"Bus {self.name} \nmax speed: {self.max_speed} \ntotal capacity: {self.total_capacity} \nused capacity: {self.used_capacity}"
+#5
     def fare(self):
         return self.total_capacity * 100 * 1,1
 
-class Car(Vehicle):
+#8
+class Engine:
+    def __init__(self, volume):
+        self.volume = volume
+
+    def get_volume(self):
+        return self.volume
+
+
+class Car(Vehicle, Engine): #9
     def __init__(self, name, max_speed, total_capacity):
         super().__init__(name, max_speed, total_capacity)
+
+    def __str__(self):
+        return f"Car {self.name} \nmax speed: {self.max_speed} \ntotal capacity: {self.total_capacity}"
+
+
     pass
 
+#3
 car_1 = Car('Audi', 180, 30)
 car_2 = Car('Volvo', 200, 40)
 car_3 = Car('Opel', 230, 50)
 
-bus_1 = Bus('Mercedes', 300, 60)
-bus_2 = Bus('Renault', 310, 65)
+bus_1 = Bus('Mercedes', 300, 60, 50)
+bus_2 = Bus('Renault', 310, 65, 60)
+
 
 def typeCheck(x, checkedType):
     print(isinstance(x, checkedType))
-
+#4
 typeCheck(car_1, Car)      # True
 typeCheck(car_2, Vehicle)  # True
 typeCheck(bus_1, Car)      # False
 typeCheck(bus_1, Vehicle)  # True
+#10
+print(Car.mro())
 
+print(car_1)
+print(car_2)
+print(car_3)
+print(bus_1)
+print(bus_2)
 
