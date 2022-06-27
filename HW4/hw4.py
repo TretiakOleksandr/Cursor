26ae6b44965724db0ef37357879c052229ef8f6b
Tretiak Oleksandr <zukaramba@gamil.com>
2022-06-27 13:38:41 +0300

    +6

--- HW4/hw4.py
+++ HW4/hw4.py
@@ -10,12 +10,20 @@ class Vehicle:
 
 #2
 class Bus(Vehicle):
+    # used_capacity: int
+    message = "Sorry, too big quantity"
     def __init__(self, name, max_speed, total_capacity, used_capacity): #6
         super().__init__(name, max_speed, total_capacity)
         self.used_capacity = used_capacity
 
+    # def set_used_capacity(self, usedCapacity):
+    #     if usedCapacity > self.total_capacity:
+    #         print("Sorry, this number is too big")
+    #     else:
+    #         self.used_capacity = usedCapacity
+
     def __str__(self):
-        return f"Bus {self.name} \nmax speed: {self.max_speed} \ntotal capacity: {self.total_capacity} \nused capacity: {self.used_capacity}"
+        return f"Bus {self.name} \nmax speed: {self.max_speed} \ntotal capacity: {self.total_capacity} \nused capacity: {self.used_capacity if self.used_capacity <= self.total_capacity else self.message}"
 #5
     def fare(self):
         return self.total_capacity * 100 * 1,1
@@ -45,7 +53,9 @@ car_2 = Car('Volvo', 200, 40)
 car_3 = Car('Opel', 230, 50)
 
 bus_1 = Bus('Mercedes', 300, 60, 50)
-bus_2 = Bus('Renault', 310, 65, 60)
+# bus_1.set_used_capacity(50)
+bus_2 = Bus('Renault', 310, 65, 70)
+# bus_2.set_used_capacity(60)
 
 
 def typeCheck(x, checkedType):
